82b9b7fffd207d75ad385e39d8df02d54d74f19b
Jayee <jayee.jiayi.fu@gmail.com>
2023-08-13 20:42:28 +0800

    第4章-分支基本用法

---
+++ learning/条件分支控制流/修改对象的布尔值.py
@@ -0,0 +1,44 @@
+# 某个对象用于分支判断时，解释器会对它进行“真值测试”，计算出它的布尔值
+# 默认情况下，所有用户自定义的类和类实例的计算结果都是True
+class Foo():
+    pass
+print(f'bool(Foo): {bool(Foo)}')
+
+
+class UserCollection():
+    def __init__(self, users):
+        self.items = users
+users1 = []
+users2 = [1,2]
+print(f'users1 = [], bool(UserCollection(users1)): { bool(UserCollection(users1)) }')
+print(f'users2 = [1, 2], bool(UserCollection(users2)): { bool(UserCollection(users2)) }')
+
+
+# 1. 修改__len__
+# 为类定义__len__魔法方法，实际上就是为它实现了Python世界的长度协议
+# Python在计算这类对象的布尔值时，会受len(users)的结果影响
+class UserCollection_len():
+    def __init__(self,users):
+        self.items = users
+    def __len__(self):
+        return len(self.items)
+users1 = []
+users2 = [1,2]
+print(f'users1 = [], bool(UserCollection_len(users1)): { bool(UserCollection_len(users1)) }')
+print(f'users2 = [1, 2], bool(UserCollection_len(users2)): { bool(UserCollection_len(users2)) }')
+
+
+# 2. 修改 __bool__
+# 为对象定义__bool__方法后，对它进行布尔值运算会直接返回该方法的调用结果
+# 同时定义了__len__和__bool__两个方法，解释器会优先使用__bool__方法的执行结果
+class UserCollection_bool():
+    def __init__(self,users):
+        self.items = users
+    def __len__(self):
+        return len(self.items)
+    def __bool__(self):
+        return False
+users1 = []
+users2 = [1,2]
+print(f'users1 = [], bool(UserCollection_bool(users1)): { bool(UserCollection_bool(users1)) }')
+print(f'users2 = [1, 2], bool(UserCollection_bool(users2)): { bool(UserCollection_bool(users2)) }')
---
+++ learning/条件分支控制流/等值判断==和is.py
@@ -0,0 +1,52 @@
+# 1. 双等号运算符==，它会对比两个值是否一致
+x, y, z = 1,1,3
+print(f'x==y : {x ==y}, x == z : {x == z}')
+
+
+# 2. 类的 == 结果可操控，修改 __eq__方法
+class EqualWithAnything():
+    '''与任何对象相等'''
+    def __eq__(self, other):
+        '''other 是 == 右边的对象'''
+        return True
+foo = EqualWithAnything()
+print(f'foo == False: {foo == False}')
+print(f'foo == None: {foo == None}')
+
+
+# 3. 严格检查某个对象为None
+# 在执行x is y时，其实就是在判断id(x)和id(y)的结果是否相等，二者是否是同一个对象。
+# （1）==对比两个对象的值是否相等，行为可被__eq__方法重载；
+# （2）is判断两个对象是否是内存里的同一个东西，无法被重载。
+print(f'foo is None: {foo is None}')
+x = None
+print(f'x is None: {x is None}')
+
+# 注意：
+# 除了None、True和False这三个内置对象以外，其他类型的对象在Python中并不是严格以单例模式存在的。
+# 换句话说，即便值一致，它们在内存中仍然是完全不同的两个对象。
+
+
+# 4. 驻留技术，对不变类型数据有效
+# 一种底层优化技术，缓存在内存里的一个数组中，需要时从缓存返回，而不是新建对象
+x = 123445
+y = 123445
+print(f'x = {x}, id(x): {id(x)}, y = {y}, id(y): {id(y)}')
+print(f'x is y: {x is y}')
+
+
+x = 'abcdefg'
+y = 'abcdefg'
+print(f'x = {x}, id(x): {id(x)}, y = {y}, id(y): {id(y)}')
+print(f'x is y: {x is y}')
+
+x = (1,2,3)
+y = (1,2,3)
+print(f'x = {x}, id(x): {id(x)}, y = {y}, id(y): {id(y)}')
+print(f'x is y: {x is y}')
+
+
+x = [1,2,3]
+y = [1,2,3]
+print(f'x = {x}, id(x): {id(x)}, y = {y}, id(y): {id(y)}')
+print(f'x is y: {x is y}')
