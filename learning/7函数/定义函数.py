3723a64ca2b603276e9d35c91633da0140ebf9ee
Jayee <jayee.jiayi.fu@gmail.com>
2024-03-26 13:28:45 +0800

    第7章 第一节

--- learning/7函数/定义函数.py
+++ learning/7函数/定义函数.py
@@ -1,14 +1,19 @@
 
 # 1. 定义与调用函数
-def add(x,y):
+def add(x, y):
     return x + y
-result = add(3,4)
+
+
+result = add(3, 4)
 print(f'3 + 4 = {result}')
 
 
 # 2. lambda 定义匿名函数
-add = lambda x,y: x + y
-result = add(4,3)
+def add(x, y):
+    return x
+
+
+result = add(4, 3)
 print(f'4 + 3 = {result}')
 
 
@@ -16,13 +21,15 @@ print(f'4 + 3 = {result}')
 abs(100)
 n1 = 255
 n2 = 1000
-print(hex(n1),hex(n2))
+print(hex(n1), hex(n2))
 
 # print('-------------------')
 
 # 定义函数
 # def my_abs(x):
 #     if not isinstance(x,(int,float)):
+
+
 #         raise TypeError('bad operated type')
 #     if x >= 0 :
 #         return x
@@ -34,12 +41,11 @@ print(hex(n1),hex(n2))
 # 返回多个值
 # def move(x,y,step):
 #     nx = x + step * 2
-#     ny = y - step * 3 
+#     ny = y - step * 3
 #     return nx, ny
 # r = move(2,3,4)
 # x,y = r
 # print(r,x,y)
-
 
 
 # 递归函数
@@ -51,10 +57,15 @@ print(hex(n1),hex(n2))
 
 # 递归函数，尾递归优化
 def fat(n):
-    return fat_iter(n,1)
-def fat_iter(num,product):
+    return fat_iter(n, 1)
+
+
+def fat_iter(num, product):
     if num == 1:
         return product
+
     else:
-        return fat_iter(num-1,num*product)
-print(fat(1),fat(5))
+        return fat_iter(num-1, num*product)
+
+
+print(fat(1), fat(5))
