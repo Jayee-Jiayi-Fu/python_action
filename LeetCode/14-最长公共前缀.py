11a53206b4b28c2434aebed8a7516cdda63f111a
Jayee <jayee.jiayi.fu@gmail.com>
2024-06-08 10:55:03 +0800

    14-最长公共前缀

---
+++ LeetCode/14-最长公共前缀.py
@@ -0,0 +1,18 @@
+14-最长公共前缀.py
+# 解题内容
+
+
+def test_func(s):
+
+    return True
+
+
+# 测试数据
+CASES = [
+    {
+        'input_data': ["flower", "flow", "flight"],
+        'output_data':"fl"
+    }, {
+        'input_data': ["dog", "racecar", "car"],
+        'output_data':""
+    }, ]
