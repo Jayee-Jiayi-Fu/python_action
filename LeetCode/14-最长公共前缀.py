afdd6769b5061f77ea37e1c00e19838d84b730a0
Jayee <jayee.jiayi.fu@gmail.com>
2024-06-15 23:21:08 +0800

    temp1

--- LeetCode/14-最长公共前缀.py
+++ LeetCode/14-最长公共前缀.py
@@ -1,18 +1,2 @@
-14-最长公共前缀.py
-# 解题内容
-
-
-def test_func(s):
-
-    return True
-
-
-# 测试数据
-CASES = [
-    {
-        'input_data': ["flower", "flow", "flight"],
-        'output_data':"fl"
-    }, {
-        'input_data': ["dog", "racecar", "car"],
-        'output_data':""
-    }, ]
+s = [1, 2, 3]
+'-'.join(s)
--- LeetCode/common.py
+++ LeetCode/common.py
@@ -1,18 +1,47 @@
 
-def test_func(s):
 
-    return True
+def test_func(input_data):
+    haystack = input_data['haystack']
+    needle = input_data['needle']
+
+    # if haystack == needle:
+    #     return 0
+
+    len_haystack = len(haystack)
+    len_needle = len(needle)
+
+    if len_haystack < len_needle:
+        return -1
+
+    for i in range(len_haystack - len_needle + 1):
+        # print('test info:', i, haystack[i: i + len_needle])
+        if haystack[i: i + len_needle] == needle:
+            return i
+    return -1
 
 
 # 测试数据
-CASES = [
-    {
-        'input_data': ["flower", "flow", "flight"],
-        'output_data':"fl"
-    }, {
-        'input_data': ["dog", "racecar", "car"],
-        'output_data':""
-    }, ]
+CASES = [{
+    'input_data': {
+        'haystack': "sadbutsad",
+        'needle': "sad"},
+    'output_data': 0
+}, {
+    'input_data': {
+        'haystack': "leetcode",
+        'needle': "leeto"},
+    'output_data': -1
+}, {
+    'input_data': {
+        'haystack': "a",
+        'needle': "a"},
+    'output_data': 0
+}, {
+    'input_data': {
+        'haystack': "abc",
+        'needle': "c"},
+    'output_data': 2
+}]
 
 
 # 测试所有用例
@@ -44,4 +73,4 @@ def run_test_func(case_num):
 
 
 test_cases()
-# test_case(0)
+# test_case(3)
